d653ac3778021a25c59b8ced4ca452c91ae3e811
jk <akdirdl@gmail.com>
2024-07-11 01:39:02 +0900

    movie detail & css

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,5 +1,5 @@
-import { API_URL } from "../app/(home)/page";
-
+import { API_URL } from "../app/constants";
+import styles from "../styles/movie-vidoes.module.css";
 
 async function getVideos(id: string){
     const response = await fetch(`${API_URL}/${id}/videos`);
@@ -8,5 +8,15 @@ async function getVideos(id: string){
 
 export default async function MovieVideos({id} : {id:string}){
     const videos = await getVideos(id);
-    return <h6>{JSON.stringify(videos)}</h6>;
+    return (
+    <div className={styles.container}>
+        {videos.map((video) => (
+        <iframe 
+            key={video.id} 
+            src={`https://youtube.com/embed/${video.key}`} 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title={video.name} />))}
+    </div>
+    );
 }
